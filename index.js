24744764cc80a674bc29480ad99145285c39766a
Bishal Arman <www.bishalarman43@gmail.com>
2023-11-23 05:19:09 +0600

    feat:mongodb connect with mongoose

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/practice").then(()=>{
+    console.log("mongodb connected successfully");
+}).catch((err)=>{
+    console.log(err);
+})
+
+const student=new mongoose.Schema({
+    name:String,
+    workout:Boolean,
+    height:Number
+})
+
+const Student=new mongoose.model("students",student);
+
+const adder=async()=>{
+  const ss=await Student.create({
+    
+        name:"bishal",
+        workout:true,
+        height:8
+    
+  })
+}
+
+adder()
